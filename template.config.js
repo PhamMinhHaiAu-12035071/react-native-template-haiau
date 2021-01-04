5f289e7a127b01ed957cb33aeef0e0e05142c75a
aupmh <phamau>
2021-01-04 21:00:13 +0700

    feat: handle script

--- template.config.js
+++ template.config.js
@@ -7,5 +7,5 @@ module.exports = {
     templateDir: './template',
 
     // Path to script, which will be executed after init
-    postInitScript: './script.js'
+    postInitScript: './template/scripts/init/script.js'
 };
